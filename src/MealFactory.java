b3298e0de0396c50aad0a5bed7f940ce233fd935
Huijun Hu <huijunhu@cpp.edu>
2024-05-16 14:34:01 -0700

    combine meal and diet

--- src/MealFactory.java
+++ src/MealFactory.java
@@ -1,9 +1,19 @@
 
 // Factory Pattern
-import java.util.List;
-
 class MealFactory {
-    public static Meal createMeal(String name, int fats, int carbs, int protein) {
-        return new Meal(name, fats, carbs, protein);
+    public static MealComponent createMeal(String dietPlan) {
+        switch (dietPlan) {
+            case "noRestriction":
+                return new noDietRestriction("No Restriction Meal");
+            case "vegan":
+                return new veganDiet("Vegan Meal");
+            case "poleo":
+                return new paleoDiet("Paleo Meal");
+            case "nutAllergy":
+                return new nutAllergyDiet("Nut Allergy Meal");
+            default:
+                return new noDietRestriction("No Restriction Meal");
+        }
+
     }
 }
