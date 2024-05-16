ee71c9b6c9b5eb90dc12a8164d14b62b54f430e7
Nil Patel <nilrnp@gmail.com>
2024-05-15 20:24:19 -0700

    removed shifttypes

--- src/Driver.java
+++ src/Driver.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Driver extends User {
-    private ShiftType shift;
+    private List<String> shiftOptions = new ArrayList<>(List.of("12AM - 8AM","8AM - 4PM","4PM - 12AM"));
+    private String shift;
     private List<Order> orders;
 
-    public Driver(String name, String address, String county, ShiftType shift) {
+    public Driver(String name, String address, String county, int shift) {
         super(name, address, county);
-        this.shift = shift;
+        this.shift = shiftOptions.get(shift);
         this.orders = new ArrayList<>();
     }
 
-    public ShiftType getShift() {
+    public String getShift() {
         return shift;
     }
 
